bf766e5354ad2b727287f1c44071f916487ee778
root <you@example.com>
2023-02-12 14:00:39 -0800

    task updated

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python3
-# __init__.py
-
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 11 13:56:43 2020
+@authors: Oluwaseun Adesiyan
+          Teslim Okunola
+"""
 from models.engine.file_storage import FileStorage
+
+
 storage = FileStorage()
 storage.reload()
